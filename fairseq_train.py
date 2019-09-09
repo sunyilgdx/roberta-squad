2ee45561f5e145d360d1bab6c8ee4ad92a41c594
ecchochan <ecchochan@gmail.com>
2019-09-09 09:31:11 +0800

    Add files via upload

---
+++ fairseq_train.py
@@ -0,0 +1,517 @@
+from fairseq import cli_main
+
+
+import os
+import numpy as np
+import torch
+from fairseq.data import (
+    data_utils,
+    Dictionary,
+    encoders,
+)
+from fairseq.tasks import FairseqTask, register_task
+from fairseq.criterions import FairseqCriterion, register_criterion
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from fairseq import utils
+from fairseq.models import (
+    FairseqDecoder,
+    FairseqLanguageModel,
+    register_model,
+    register_model_architecture,
+)
+
+from fairseq.data import (
+    data_utils,
+    Dictionary,
+    encoders,
+    IdDataset,
+    ListDataset,
+    NestedDictionaryDataset,
+    NumSamplesDataset,
+    NumelDataset,
+    PadDataset,
+    SortDataset,
+)
+from fairseq.modules import (
+    LayerNorm,
+    TransformerSentenceEncoder,
+)
+from fairseq.modules.transformer_sentence_encoder import init_bert_params
+
+from .hub_interface import RobertaHubInterface
+
+import argparse
+from fairseq.data import Dictionary
+from fairseq.optim.fp16_optimizer import MemoryEfficientFP16Optimizer
+from tokenizer.roberta import RobertaTokenizer, MASKED, NOT_MASKED, IS_MAX_CONTEXT, NOT_IS_MAX_CONTEXT
+from glob import glob
+import numpy as np
+from torch.nn import CrossEntropyLoss
+from ranger import Ranger
+from ranger import AdamW
+import json
+from tokenizer.validate import validate
+from copy import deepcopy
+from time import time
+from multiprocessing import Pool
+import multiprocessing
+import gc
+import random
+from tqdm import tqdm
+import os
+
+roberta_directory = './roberta.large'
+
+
+max_seq_length   = 512
+max_query_length = 128
+doc_stride       = 128
+
+default_choices = []
+get_tokenizer = lambda: RobertaTokenizer(config_dir=roberta_directory)
+
+tk = tokenizer =  get_tokenizer()
+
+
+#Data Utilities
+
+import marshal
+def read(dat):
+    uid, inp, start, end, p_mask, unanswerable = marshal.loads(dat)
+    inp = np.frombuffer(inp, dtype=np.uint16).astype(np.int32)
+    p_mask = np.frombuffer(p_mask, dtype=np.bool).astype(np.float32)
+    return uid, inp, start, end, p_mask, unanswerable
+
+def fread(f):
+    uid, inp, start, end, p_mask, unanswerable = marshal.load(f)
+    inp = np.frombuffer(inp, dtype=np.uint16).astype(np.int32)
+    p_mask = np.frombuffer(p_mask, dtype=np.bool).astype(np.float32)
+    return uid, inp, start, end, p_mask, unanswerable
+            
+         
+def pad(list_of_tokens, 
+        dtype=np.long,
+        torch_tensor=None,
+        pad_idx=1):
+    k = np.empty((len(list_of_tokens),max_seq_length), dtype=dtype)
+    k.fill(pad_idx)
+    i = 0
+    for tokens in list_of_tokens:
+        k[i,:len(tokens)] = tokens
+        i += 1
+    return k if torch_tensor is None else torch_tensor(k)
+
+from torch.utils.data.dataset import Dataset
+
+def from_records(records):
+	"""
+	Args:
+		records (string): Path to the csv file with annotations.
+	"""
+  
+	fn_style = isinstance(records,str)
+	if fn_style:
+	  def from_file(fn):
+		with open(fn, 'rb') as f:
+			while True:
+				try:
+					record = fread(f)
+					yield record
+				except EOFError:
+					break
+	  records = from_file(records)
+
+	records = list(records)
+	  
+	prepared_records = []
+	for record_samples in chunks(records,batch_size):
+		uid, inp, start, end, p_mask, unanswerable = zip(*record_samples) if fn_style else zip(*(read(record) for record in record_samples))
+		start = start
+		end = end
+		unanswerable = unanswerable
+		inp = pad(inp,dtype=np.long)
+		p_mask = pad(p_mask,dtype=np.float32)
+
+		for e in zip(inp, p_mask, start, end, unanswerable):
+			yield e
+
+
+
+
+
+
+
+
+
+
+
+
+@register_model('roberta_qa')
+class RobertaQAModel(FairseqLanguageModel):
+
+    @classmethod
+    def hub_models(cls):
+        return {
+            'roberta.base': 'http://dl.fbaipublicfiles.com/fairseq/models/roberta.base.tar.gz',
+            'roberta.large': 'http://dl.fbaipublicfiles.com/fairseq/models/roberta.large.tar.gz',
+            'roberta.large.mnli': 'http://dl.fbaipublicfiles.com/fairseq/models/roberta.large.mnli.tar.gz',
+            'roberta.large.wsc': 'http://dl.fbaipublicfiles.com/fairseq/models/roberta.large.wsc.tar.gz',
+        }
+
+    def __init__(self, args, encoder):
+        super().__init__(encoder)
+        self.args = args
+
+        # We follow BERT's random weight initialization
+        self.apply(init_bert_params)
+
+
+    @staticmethod
+    def add_args(parser):
+        """Add model-specific arguments to the parser."""
+        parser.add_argument('--encoder-layers', type=int, metavar='L',
+                            help='num encoder layers')
+        parser.add_argument('--encoder-embed-dim', type=int, metavar='H',
+                            help='encoder embedding dimension')
+        parser.add_argument('--encoder-ffn-embed-dim', type=int, metavar='F',
+                            help='encoder embedding dimension for FFN')
+        parser.add_argument('--encoder-attention-heads', type=int, metavar='A',
+                            help='num encoder attention heads')
+        parser.add_argument('--activation-fn',
+                            choices=utils.get_available_activation_fns(),
+                            help='activation function to use')
+        parser.add_argument('--pooler-activation-fn',
+                            choices=utils.get_available_activation_fns(),
+                            help='activation function to use for pooler layer')
+        parser.add_argument('--encoder-normalize-before', action='store_true',
+                            help='apply layernorm before each encoder block')
+        parser.add_argument('--dropout', type=float, metavar='D',
+                            help='dropout probability')
+        parser.add_argument('--attention-dropout', type=float, metavar='D',
+                            help='dropout probability for attention weights')
+        parser.add_argument('--activation-dropout', type=float, metavar='D',
+                            help='dropout probability after activation in FFN')
+        parser.add_argument('--pooler-dropout', type=float, metavar='D',
+                            help='dropout probability in the masked_lm pooler layers')
+        parser.add_argument('--max-positions', type=int,
+                            help='number of positional embeddings to learn')
+        parser.add_argument('--load-checkpoint-heads', action='store_true',
+                            help='(re-)register and load heads when loading checkpoints')
+
+    @classmethod
+    def build_model(cls, args, task):
+        """Build a new model instance."""
+
+        # make sure all arguments are present
+        base_architecture(args)
+
+        if not hasattr(args, 'max_positions'):
+            args.max_positions = args.tokens_per_sample
+
+        encoder = RobertaQAEncoder(args, task.source_dictionary)
+        return cls(args, encoder)
+
+    def forward(self, src_tokens, features_only=False, return_all_hiddens=False, classification_head_name=None, **kwargs):
+        if classification_head_name is not None:
+            features_only = True
+
+        x, extra = self.decoder(src_tokens, features_only, return_all_hiddens, **kwargs)
+
+        return x, extra
+
+    @property
+    def supported_targets(self):
+        return {'self'}
+
+    @classmethod
+    def from_pretrained(cls, model_name_or_path, checkpoint_file='model.pt', data_name_or_path='.', bpe='gpt2', **kwargs):
+        from fairseq import hub_utils
+        x = hub_utils.from_pretrained(
+            model_name_or_path,
+            checkpoint_file,
+            data_name_or_path,
+            archive_map=cls.hub_models(),
+            bpe=bpe,
+            load_checkpoint_heads=True,
+            **kwargs,
+        )
+        return RobertaHubInterface(x['args'], x['task'], x['models'][0])
+
+
+
+class PoolerAnswerClass(nn.Module):
+    """ Compute SQuAD 2.0 answer class from classification and start tokens hidden states. """
+    def __init__(self, hidden_size):
+        super(PoolerAnswerClass, self).__init__()
+        self.dense_0 = nn.Linear(hidden_size, hidden_size)
+        self.activation = nn.Tanh() #Mish() # nn.Tanh()
+        #self.dropout = nn.Dropout(p=dropout)
+        self.dense_1 = nn.Linear(hidden_size, 1)
+
+    def forward(self, hidden_states, cls_index=None):
+        """
+        Args:
+            One of ``start_states``, ``start_positions`` should be not None.
+            If both are set, ``start_positions`` overrides ``start_states``.
+            **cls_index**: torch.LongTensor of shape ``(batch_size,)``
+                position of the CLS token. If None, take the last token.
+
+            note(Original repo):
+                no dependency on end_feature so that we can obtain one single `cls_logits`
+                for each sample
+        """
+
+        if cls_index is not None:
+            cls_index = cls_index[:, None, None].expand(-1, -1, hsz) # shape (bsz, 1, hsz)
+            cls_token_state = hidden_states.gather(-2, cls_index).squeeze(-2) # shape (bsz, hsz)
+        else:
+            cls_token_state = hidden_states[:, 0, :] # shape (bsz, hsz)
+
+
+        x = self.dense_0(cls_token_state)
+        x = self.activation(x)
+        x = self.dense_1(x).squeeze(-1)
+
+        return x
+
+
+class RobertaQAEncoder(FairseqDecoder):
+    """RoBERTa encoder.
+    Implements the :class:`~fairseq.models.FairseqDecoder` interface required
+    by :class:`~fairseq.models.FairseqLanguageModel`.
+    """
+
+    def __init__(self, args, dictionary):
+        super().__init__(dictionary)
+        self.args = args
+        self.sentence_encoder = TransformerSentenceEncoder(
+            padding_idx=dictionary.pad(),
+            vocab_size=len(dictionary),
+            num_encoder_layers=args.encoder_layers,
+            embedding_dim=args.encoder_embed_dim,
+            ffn_embedding_dim=args.encoder_ffn_embed_dim,
+            num_attention_heads=args.encoder_attention_heads,
+            dropout=args.dropout,
+            attention_dropout=args.attention_dropout,
+            activation_dropout=args.activation_dropout,
+            max_seq_len=args.max_positions,
+            num_segments=0,
+            encoder_normalize_before=True,
+            apply_bert_init=True,
+            activation_fn=args.activation_fn,
+        )
+        self.span_logits =  nn.Linear(args.hidden_size, 2)
+        self.answer_class = PoolerAnswerClass(args.hidden_size)
+
+    def forward(self, src_tokens, features_only=False, return_all_hiddens=False, **unused):
+        x, extra = self.extract_features(src_tokens, return_all_hiddens)
+        if not features_only:
+            start_logits, end_logits = self.span_logits(x).split(1, dim=-1)
+            cls_logits = self.answer_class(x, cls_index=cls_index)
+			x = (start_logits, end_logits, cls_logits)
+			
+			
+        return x, extra
+
+    def extract_features(self, src_tokens, return_all_hiddens=False, **unused):
+        inner_states, _ = self.sentence_encoder(
+            src_tokens, last_state_only=not return_all_hiddens,
+        )
+        features = inner_states[-1]
+        return features, {'inner_states': inner_states if return_all_hiddens else None}
+
+    def max_positions(self):
+        """Maximum output length supported by the encoder."""
+        return self.args.max_positions
+
+
+
+@register_model_architecture('roberta_qa', 'roberta_qa')
+def base_architecture(args):
+    args.encoder_layers = getattr(args, 'encoder_layers', 12)
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 768)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 3072)
+    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 12)
+
+    args.activation_fn = getattr(args, 'activation_fn', 'gelu')
+    args.pooler_activation_fn = getattr(args, 'pooler_activation_fn', 'tanh')
+
+    args.dropout = getattr(args, 'dropout', 0.1)
+    args.attention_dropout = getattr(args, 'attention_dropout', 0.1)
+    args.activation_dropout = getattr(args, 'activation_dropout', 0.0)
+    args.pooler_dropout = getattr(args, 'pooler_dropout', 0.0)
+
+
+@register_model_architecture('roberta_qa', 'roberta_qa_large')
+def roberta_large_architecture(args):
+    args.encoder_layers = getattr(args, 'encoder_layers', 24)
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 1024)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 4096)
+    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 16)
+    base_architecture(args)
+
+
+
+
+@register_task('squad2')
+class SQuAD2Task(FairseqTask):
+    """Task for training masked language models (e.g., BERT, RoBERTa)."""
+
+    @staticmethod
+    def add_args(parser):
+        """Add task-specific arguments to the parser."""
+        parser.add_argument('data', help='colon separated path to data directories list, \
+                            will be iterated upon during epochs in round-robin manner')
+        parser.add_argument('--default_choices', default='', type=str)
+		#max_positions
+
+    def __init__(self, args, dictionary):
+        super().__init__(args)
+
+    @classmethod
+    def setup_task(cls, args, **kwargs):
+        paths = args.data.split(':')
+        assert len(paths) > 0
+        dictionary = Dictionary.load(os.path.join(paths[0], 'dict.txt'))
+        print('| dictionary: {} types'.format(len(dictionary)))
+        return cls(args, dictionary)
+
+    def load_dataset(self, split, epoch=0, combine=False):
+        """Load a given dataset split.
+        Args:
+            split (str): name of the split (e.g., train, valid, test)
+        """
+        path = self.args.data
+
+		tokens = []
+		starts = []
+		ends = []
+		unanswerables = []
+		
+		lengths = []
+		
+		for inp, p_mask, start, end, unanswerable in from_records(path):
+			tokens.append(inp)
+			lengths.append(len(inp))
+			starts.append(start)
+			ends.append(end)
+			unanswerables.append(unanswerable)
+			
+		
+        tokens = ListDataset(tokens, lengths)
+        starts = ListDataset(starts, [1]*len(starts))
+        ends = ListDataset(ends, [1]*len(ends))
+        unanswerables = ListDataset(unanswerables, [1]*len(unanswerables))
+
+
+        print('| loaded {} batches from: {}'.format(len(dataset), path))
+
+        shuffle = np.random.permutation(len(dataset))
+
+        self.datasets[split] = SortDataset(
+            NestedDictionaryDataset(
+                {
+                    'id': IdDataset(),
+                    'tokens': tokens,
+                    'starts': starts,
+                    'ends': ends,
+                    'unanswerables': unanswerables,
+					'nsentences': NumSamplesDataset(),,
+					'ntokens': NumelDataset(tokens, reduce=True),
+                },
+                sizes=[lengths],
+            ),
+            sort_order=[
+                shuffle,
+            ],
+        )
+
+    @property
+    def source_dictionary(self):
+        return self.dictionary
+
+    @property
+    def target_dictionary(self):
+        return self.dictionary
+
+
+@register_criterion('squad2')
+class SQuAD2Criterion(FairseqCriterion):
+
+    def __init__(self, args, task):
+        super().__init__(args, task)
+        if self.args.save_predictions is not None:
+            self.prediction_h = open(self.args.save_predictions, 'w')
+        else:
+            self.prediction_h = None
+
+    def __del__(self):
+        if self.prediction_h is not None:
+            self.prediction_h.close()
+
+    @staticmethod
+    def add_args(parser):
+        # fmt: off
+        parser.add_argument('--save-predictions', metavar='FILE',
+                            help='file to save predictions to')
+        # fmt: on
+
+    def forward(self, model, sample, reduce=True):
+        # compute loss and accuracy
+		tokens = sample['tokens']
+		unanswerable = sample['unanswerables']
+		start_positions = sample['starts']
+		end_positions = sample['ends']
+		
+        (start_logits, end_logits, cls_logits), extra = model(tokens)
+		
+		
+		for x in (start_positions, end_positions, unanswerable):
+			if x is not None and x.dim() > 1:
+				x.squeeze_(-1)
+
+		loss_fct = CrossEntropyLoss()
+		start_loss = loss_fct(start_logits, start_positions)
+		end_loss = loss_fct(end_logits, end_positions)
+		total_loss = (start_loss + end_loss) / 2
+
+
+		loss_fct_cls = nn.BCEWithLogitsLoss()
+		cls_loss = loss_fct_cls(cls_logits, unanswerable)
+		
+		total_loss += cls_loss * 0.5
+
+
+
+        sample_size = tokens.size(0) 
+        logging_output = {
+            'loss': utils.item(total_loss.data) if reduce else total_loss.data,
+            'ntokens': sample['ntokens'],
+            'nsentences': sample['nsentences'],
+            'sample_size': sample_size,
+        }
+        return total_loss, sample_size, logging_output
+
+    @staticmethod
+    def aggregate_logging_outputs(logging_outputs):
+        """Aggregate logging outputs from data parallel training."""
+        loss_sum = sum(log.get('loss', 0) for log in logging_outputs)
+        ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
+        nsentences = sum(log.get('nsentences', 0) for log in logging_outputs)
+        sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
+
+        agg_output = {
+            'loss': loss_sum / sample_size / math.log(2),
+            'ntokens': ntokens,
+            'nsentences': nsentences,
+            'sample_size': sample_size,
+        }
+
+        return agg_output
+		
+		
+		
+cli_main()
