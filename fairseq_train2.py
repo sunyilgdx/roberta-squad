f0ad6d4c380c67a82aac27925539c4e0d8415722
ecchochan <ecchochan@gmail.com>
2019-09-21 17:28:13 +0800

    Add files via upload

--- fairseq_train2.py
+++ fairseq_train2.py
@@ -1342,11 +1342,11 @@ class RobertaQAEmbed(FairseqDecoder):
         self.a_fnn_layer = FnnLayer(hs)
         
     def get_pooled_output_first_token_from_layers(self, x, layers=-4):
-        return torch.stack(self.sentence_encoder(q, last_state_only=False)[0][layers:]).mean(0)[0,:,:]
+        return torch.stack(self.sentence_encoder(x, last_state_only=False)[0][layers:]).mean(0)[0,:,:]
     def get_pooled_output_first_token_from_last_layer(self, x):
-        return self.sentence_encoder(q, last_state_only=False)[0][-1].mean(0)[0,:,:]
+        return self.sentence_encoder(x, last_state_only=False)[0][-1].mean(0)[0,:,:]
     def get_pooled_output_average_tokens_from_last_layer(self, x):
-        return self.sentence_encoder(q, last_state_only=True)[0][-1].mean(0) * torch.transpose(q.eq(self.sentence_encoder.padding_idx).unsqueeze(-1).type_as(q_hs), 1,0).mean(0)
+        return self.sentence_encoder(x, last_state_only=True)[0][-1].mean(0) * torch.transpose(x.eq(self.sentence_encoder.padding_idx).unsqueeze(-1).type_as(q_hs), 1,0).mean(0)
 
     def forward(self, q=None, a=None, return_loss=False, **kwargs):
         has_q = q is not None
