292aed8a4201107cf2938b6e6b7df27b792b7e86
ecchochan <ecchochan@gmail.com>
2019-09-21 17:38:20 +0800

    Add files via upload

--- fairseq_train2.py
+++ fairseq_train2.py
@@ -1346,7 +1346,7 @@ class RobertaQAEmbed(FairseqDecoder):
     def get_pooled_output_first_token_from_last_layer(self, x):
         return self.sentence_encoder(x, last_state_only=False)[0][-1].mean(0)[0,:,:]
     def get_pooled_output_average_tokens_from_last_layer(self, x):
-        y = self.sentence_encoder(x, last_state_only=True)[0][-1].mean(0)  #  B, T, C
+        y = self.sentence_encoder(x, last_state_only=True)[0][-1].mean(1)  #  B, T, C
         return ( y * x.eq(self.sentence_encoder.padding_idx).unsqueeze(-1).type_as(y)).mean(0)
 
     def forward(self, q=None, a=None, return_loss=False, **kwargs):
